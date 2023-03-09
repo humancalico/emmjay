b84cada31ef0245579a5b4aabbfb21c01c976654
Akshat Agarwal <hey@akshat.dev>
2023-03-09 12:44:29 +0530

    refactor: use FlatList to render attendance cards

--- Components/Attendance.js
+++ Components/Attendance.js
@@ -3,68 +3,52 @@ import Card from './Card'
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import { FlatList } from 'react-native';
 
-export default function Attendance({navigation}) {
+const cardsData = [
+  { id: '1', heading: 'Mathematics', ratio: '7/10', status: 'Attend all the classes it is very necessary otherwise you will faildsfjklasdf;ja;' },
+  { id: '2', heading: 'Science', ratio: '5/10', status: 'Attend all the classes it is very necessary otherwise you will faildsfjklasdf;ja;' },
+  { id: '3', heading: 'History', ratio: '8/10', status: 'Attend all the classes it is very necessary otherwise you will faildsfjklasdf;ja;' },
+  { id: '4', heading: 'English', ratio: '4/10', status: 'Attend all the classes it is very necessary otherwise you will faildsfjklasdf;ja;' },
+  { id: '5', heading: 'Art', ratio: '6/10', status: 'Attend all the classes it is very necessary otherwise you will faildsfjklasdf;ja;' },
+];
+
+const renderItem = ({ item }) => (
+  <Card Heading={item.heading} Ratio={item.ratio} Status={item.status} />
+);
+
+const AttendanceListComponent = () => {
+  return (
+    <FlatList
+      data={cardsData}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+    />
+  )
+}
+
+export default function Attendance({ navigation }) {
   return (
     <View style={styles.container}>
       {/* <Button title="Visit Again" onPress={()=>navigation.navigate('Attendance')}>
       </Button> */}
-    <ScrollView contentContainerStyle={{
-    //   alignItems: "center",
-    //   justifyContent: "center"
-    }}>
-      <Card
-        Heading="Software Development"
-        Ratio="7/10"
-        Status="On Track, you can skip the next 2 classes"
-      ></Card>
-      <Card
-        Heading="ANM"
-        Ratio="3/10"
-        Status="Attend all the classes it is very necessaraerea otherwise you will faildsfjklasdf;ja;"
-      ></Card>
-      <Card
-        Heading="ANM"
-        Ratio="3/10"
-        Status="Attend all the classes it is very necessaraerea otherwise you will faildsfjklasdf;ja;"
-      ></Card>
-      <Card
-        Heading="ANM"
-        Ratio="3/10"
-        Status="Attend all the classes it is very necessaraerea otherwise you will faildsfjklasdf;ja;"
-      ></Card>
-      <Card
-        Heading="ANM"
-        Ratio="3/10"
-        Status="Attend all the classes it is very necessaraerea otherwise you will faildsfjklasdf;ja;"
-      ></Card>
-      <Card
-        Heading="ANM"
-        Ratio="3/10"
-        Status="Attend all the classes it is very necessaraerea otherwise you will faildsfjklasdf;ja;"
-      ></Card>
-      <Card
-        Heading="ANM"
-        Ratio="3/10"
-        Status="Attend all the classes it is very necessaraerea otherwise you will faildsfjklasdf;ja;"
-      ></Card>
-      <Card
-        Heading="ANM"
-        Ratio="3/10"
-        Status="Attend all the classes it is very necessaraerea otherwise you will faildsfjklasdf;ja;"
-      ></Card>
-      {/* <Login /> */}
-      <StatusBar style="auto" />
-    </ScrollView>
+      <ScrollView contentContainerStyle={{
+        //   alignItems: "center",
+        //   justifyContent: "center"
+      }}>
+        <AttendanceListComponent />
+        {/* <Login /> */}
+        <StatusBar style="auto" />
+      </ScrollView>
     </View>
   )
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-      paddingTop:40
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 40
+  },
 })
