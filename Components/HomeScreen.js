9e1360b76d45ebe3828913abefe10771fadeef96
Blakelist7 <kanwal.88singh@gmail.com>
2023-05-05 11:39:07 +0530

    feat: cgpa graph

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -1,13 +1,22 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import React, { useEffect, useState } from "react";
-import { View, Text, SafeAreaView, ScrollView, TouchableOpacity, Modal, Pressable, Animated} from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+  Modal,
+  Pressable,
+  Image,
+} from "react-native";
 import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
 import SemesterScreen from "./SemesterScreen";
 import { FlatList } from "react-native";
 import { AppContext } from "../AppContext";
+import logo from "../assets/logo2.jpeg";
 
 const Tab = createMaterialBottomTabNavigator();
-
 
 // const renderItem = ({ item }) => {
 //   <SemesterScreen Heading={item.heading} />;
@@ -25,12 +34,12 @@ const Tab = createMaterialBottomTabNavigator();
 
 export default function HomeScreen({ navigation }) {
   const [modalVisible, setModalVisible] = useState(false);
-  const {studentDetail, setStudentDetail}=React.useContext(AppContext);
+  const { studentDetail, setStudentDetail } = React.useContext(AppContext);
 
-  const handleSignOut = () =>{
+  const handleSignOut = () => {
     setStudentDetail({});
-    navigation.navigate('Login');
-  }
+    navigation.navigate("Login");
+  };
 
   const now = new Date();
   let greeting = "";
@@ -58,18 +67,60 @@ export default function HomeScreen({ navigation }) {
                 setModalVisible(!modalVisible);
               }}
             >
+              <Image
+                // style={styles.image}
+                source="https://picsum.photos/seed/696/3000/2000"
+                // placeholder={blurhash}
+                contentFit="cover"
+                transition={1000}
+                style={{flex: 1,
+                  width: '100%',
+                  backgroundColor: '#0553'}}
+              /> 
               <View
-                style={{ flex: 1, backgroundColor: "#fff",marginLeft:5,marginRight:9,marginTop:10 }}
+                style={{
+                  flex: 1,
+                  backgroundColor: "#fff",
+                  marginLeft: 5,
+                  marginRight: 9,
+                  marginTop: 10,
+                }}
               >
-                <Pressable style={{alignItems:"center", justifyContent:'center', paddingVertical:15, paddingHorizontal:32, borderRadius: 4, elevation:3, backgroundColor:'black', marginBottom:12}} onPress={handleSignOut}>
-                  <Text style={{fontSize:16, lineHeight:21, fontWeight:'bold', letterSpacing:0.25, color:'white'}}>Sign out</Text>
+                <Pressable
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                    paddingVertical: 15,
+                    paddingHorizontal: 32,
+                    borderRadius: 4,
+                    elevation: 3,
+                    backgroundColor: "black",
+                    marginBottom: 12,
+                  }}
+                  onPress={handleSignOut}
+                >
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      lineHeight: 21,
+                      fontWeight: "bold",
+                      letterSpacing: 0.25,
+                      color: "white",
+                    }}
+                  >
+                    Sign out
+                  </Text>
                 </Pressable>
                 <TouchableOpacity
                   onPress={() => {
                     setModalVisible(!modalVisible);
                   }}
                 >
-                  <Entypo name="cross" size={30} style={{alignSelf:'center', marginBottom:60}}></Entypo>
+                  <Entypo
+                    name="cross"
+                    size={30}
+                    style={{ alignSelf: "center", marginBottom: 60 }}
+                  ></Entypo>
                 </TouchableOpacity>
               </View>
             </Modal>
@@ -89,9 +140,15 @@ export default function HomeScreen({ navigation }) {
             ></MaterialCommunityIcons>
           </TouchableOpacity>
         </View>
-      </ScrollView>
+      </ScrollView> 
+      <View style={{alignItems:'center',marginBottom:30,marginTop:100}}>
+          <Image source={logo} style={{ width: 200, height: 200 }} />
+        </View>
       <View style={{ flex: 1, alignItems: "center" }}>
-        <Text>{greeting}, {studentName}</Text>
+        
+        <Text>
+          {greeting}, {studentName}
+        </Text>
       </View>
     </SafeAreaView>
   );
--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -3,8 +3,8 @@ import React from 'react'
 import Attendance from '../Components/Attendance';
 import HomeScreen from '../Components/HomeScreen';
 import Grades from '../Components/Grades';
-import Profile from '../Components/Profile';
 import Search from '../Components/Search';
+import CGPA from '../Components/CGPA';
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -13,9 +13,9 @@ export default function AppStack() {
     <Tab.Navigator labeled={true} barStyle={{ backgroundColor: "white" }} activeColor="black" initialRouteName="Home" >
       <Tab.Screen name="Home1" component={HomeScreen} options={{ tabBarIcon: "home" }} ></Tab.Screen>
       <Tab.Screen name="Attendance" component={Attendance} options={{ tabBarIcon: "home" }} ></Tab.Screen>
-      <Tab.Screen name="Social" component={Search} options={{ tabBarIcon: "magnify" }} ></Tab.Screen>
+      {/* <Tab.Screen name="Social" component={Search} options={{ tabBarIcon: "magnify" }} ></Tab.Screen> */}
       <Tab.Screen name="Grades" component={Grades} options={{ tabBarIcon: "file" }} ></Tab.Screen>
-      <Tab.Screen name="Time-Table" component={Profile} options={{ tabBarIcon: "table" }} ></Tab.Screen>
+      <Tab.Screen name="CGPA" component={CGPA} options={{ tabBarIcon: "table" }} ></Tab.Screen>
     </Tab.Navigator>
   )
 }
